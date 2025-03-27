a4423cd2f77dc7b10155efa1e7e827857a651222
OneBoatSolutions <nagrajy131200350@gmail.com>
2025-03-27 11:34:04 +0530

    Added Prettier and ESLint

--- TS_Template/src/main.tsx
+++ TS_Template/src/main.tsx
@@ -7,11 +7,11 @@ import "./index.css";
 import store from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </StrictMode>
 );
